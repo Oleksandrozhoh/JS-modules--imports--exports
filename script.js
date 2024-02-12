65d2394a2698cb97f7b62c8df255b3f46b88bea3
Ozhoh, Oleksandr <oleksandrozhoh@gmail.com>
2024-02-11 20:57:30 -0600

    top level await keyword in modules

--- script.js
+++ script.js
@@ -16,3 +16,22 @@ console.log(shoppingCart.cart);
 
 import defaultValue from './shoppingCart.js';
 console.log(defaultValue);
+
+///////////////////////////////////////////////
+// top level await keyword in modules
+console.log('Start fetching');
+const responce = await fetch('https://restcountries.com/v3.1/name/usa');
+const data = await responce.json();
+console.log(data[0]);
+console.log('End of fetching');
+
+// get last post
+const lastPostData = async function () {
+  const responce = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await responce.json();
+  // console.log(data.at(-1).title);
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+// async function returns promice so we have to unpack it
+console.log(await lastPostData());
